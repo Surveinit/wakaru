6cb73ffb6e39aa77a57318a0ac4d8f059905f2f6
Surve <surve8729@gmail.com>
2025-02-06 23:28:40 +0530

    Basic http auth implementation

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,14 @@
 class AdminController < ApplicationController
+  before_action "authenticate_admin"
+
+  def index
+  end
+
+  private
+
+  def authenticate_admin
+    authenticate_or_request_with_http_basic("Admin Area") do |username, password|
+      username == "" && password == ""
+    end
+  end
 end
